52477b22f79f936178bb89fe5346346bb565ab23
Salma Ahmed <158223837+Salma-Ahmed3@users.noreply.github.com>
2024-05-28 00:43:37 +0300

    Add files via upload

---
+++ lib/Core/utils/service_locatro.dart
@@ -0,0 +1,11 @@
+// import 'package:dio/dio.dart';
+// import 'package:gbsub/Core/services/api_services.dart';
+// import 'package:gbsub/Features/instructions/repos/instruction_repo_impl.dart';
+// import 'package:get_it/get_it.dart';
+
+// final getIt = GetIt.instance;
+
+// // void setupServiceLocator() {
+// //   // ApiService(Dio());
+// //   InstructionRepoImpl(apiService: ApiService(Dio()));
+// // }
