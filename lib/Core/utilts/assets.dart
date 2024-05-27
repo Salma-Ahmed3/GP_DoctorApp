23b24f8437a27f5ad5dd1a3106348d24ef2a55ee
Salma Ahmed <158223837+Salma-Ahmed3@users.noreply.github.com>
2024-05-28 00:44:25 +0300

    Delete lib/Core/utilts directory

--- lib/Core/utilts/assets.dart
+++
@@ -1,6 +0,0 @@
-class AssetsData {
-  // ignore: constant_identifier_names
-  static const Patient = 'assets/images/patient.jpeg';
-  static const RecordPatient = 'assets/images/patient-icon.png';
-  static const DoctorHurt = 'assets/images/doctor hurt.jpeg';
-}
